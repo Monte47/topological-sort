eac701da7f82bb5445bb738a42c3a1064071a283
Monte Jiran <montejiran@gmail.com>
2017-06-19 21:30:05 -0700

    init commit

---
+++ lib/graph.rb
@@ -0,0 +1,17 @@
+class Vertex
+  def initialize(value)
+    @value = value
+    @ins = []
+    @outs = []
+  end
+end
+
+class Edge
+  def initialize(from_vertex, to_vertex, cost = 1)
+
+  end
+
+  def destroy!
+
+  end
+end
